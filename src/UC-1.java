71cbff51f15bdff3d9337478ce5818341b54b224
shoiab16 <shaikshoiabakther@gmail.com>
2026-02-16 13:07:04 +0530

    OOPS

---
+++ src/UC-1.java
@@ -0,0 +1,15 @@
+/ **
+* 00PSBannerApp UC1 - 00PS Banner Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 1.0
+*/
+
+	public class UC-1 {
+		public static void main(String[] args) {
+			System.out.println("00PS");
+		}
+	}
